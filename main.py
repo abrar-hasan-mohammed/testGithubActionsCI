c93916957a349a8411904a5423d999ea178225b8
abrarHasan77 <125270113+abrarHasan77@users.noreply.github.com>
2023-02-22 11:57:02 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-
-
 import webbrowser
 # import os
-
-
-print('Test Github actions with test case')
 class main():
     def __init__(self,url):
         self.url = url
+        print('Test Github actions with test case')
 
     def openWindow(self):
         webbrowser.open(self.url)
@@ -16,4 +12,3 @@ class main():
 
 p1 = main("http://www.microsoft.com")
 p1.openWindow()
-print('--Success--')
