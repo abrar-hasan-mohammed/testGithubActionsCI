27fb90bb84b5925d67dcfd62a4589c3d6a97f6de
abrarHasan77 <125270113+abrarHasan77@users.noreply.github.com>
2023-02-22 12:26:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-import webbrowser
-
-class main():
-    
-    
-    def __init__(self,url):
-        self.url = url
-        print('Test Github actions with test case')
-        
-        
-    def openWindow(self):
-        webbrowser.open(self.url)
-        #os.system("open \"\" https://www.google.com")
-        
-        
-p1 = main("http://www.microsoft.com")
-p1.openWindow()
+print('Hello World test with CLI')
